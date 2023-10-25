464d50775c4ca5aff7f05613751666b387b1c624
Mike <mreickcastillero@gmail.com>
2023-10-25 00:20:38 -0700

    added in all html links and index.html content

---
+++ index.js
@@ -0,0 +1,7 @@
+const git = document.getElementById("git")
+const css = document.getElementById("CSS")
+const js = document.getElementById("JS")
+const aPis = document.getElementById("APIs")
+const node = document.getElementById("Node")
+
+const links = [git, css, js, aPis, node]
